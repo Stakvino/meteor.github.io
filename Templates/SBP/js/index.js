81567dfc4e61a31188d6912bfc15eff6a8648007
Stakvino <stakvino@gmail.com>
2019-06-18 19:07:54 +0100

    SBP first example

---
+++ Templates/SBP/js/index.js
@@ -0,0 +1,21 @@
+var $ = require('./vendor/jquery-1.12.0.min.js');
+require('./plugins/jquery.jqia.slickNav.js');
+require('./plugins/jquery.jqia.slideShow.js');
+const $window = $(window);
+
+//Auto scroll plugin
+const $navBar = $("nav");
+$navBar.jqiaSlickNav();
+const $logo = $("#logo");
+const $navBarLinks = $("ul li a", $navBar);
+
+
+//heeader slider
+const $headerSlider = $("div.header-slider");
+$headerSlider.jqiaSlideShow({translateContent: true, delay: 6});
+//Resize heeader slider's height using image height
+const $sliderImg = $("img", $headerSlider).eq(0);
+$window.resize(function(){
+  const $imgHeight = $sliderImg.height();
+  $headerSlider.height($imgHeight);
+}).trigger("resize");
