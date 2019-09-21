c621efa93b23174ec849941aac652f5a7deca190
Stakvino <stakvino@gmail.com>
2019-09-21 19:20:17 +0100

    BIM2 contact 2

---
+++ Templates/BIM2/js/select-options.js
@@ -0,0 +1,11 @@
+const select_options = document.querySelector("div.select-options")
+const data_default_option = select_options.querySelector("div.data-default_option");
+const ul = select_options.querySelector("ul");
+data_default_option.addEventListener("click", e => {
+  if(ul.classList.contains("hide")){
+    ul.classList.remove("hide");
+  }else{
+    ul.classList.add("hide");
+  }
+  
+})
